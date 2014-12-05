a20f14ac0e18277dd2772d43a89bbf64ca351a57
Mitchell Lane <mitchell@nightsprout.com>
2014-12-05 12:09:12 -0500

    Performance tweaking

--- app/jobs/cache_project.rb
+++ app/jobs/cache_project.rb
@@ -3,8 +3,6 @@ class CacheProject
 
   def self.perform( project_id )
     project = Project.find( project_id )
-    ActiveRecord::Base.connection.execute("BEGIN")
     project.set_cached_sites
-    ActiveRecord::Base.connection.execute("COMMIT")
   end
 end
--- app/jobs/cache_site.rb
+++ app/jobs/cache_site.rb
@@ -3,8 +3,6 @@ class CacheSite
 
   def self.perform( site_id )
     site = Site.find( site_id )
-    ActiveRecord::Base.connection.execute("BEGIN")
     site.set_cached_projects
-    ActiveRecord::Base.connection.execute("COMMIT")
   end
 end
