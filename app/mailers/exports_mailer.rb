46479362df7fb32c4e8651f273f71e1ef9d06305
dtpowl <dtpowl@gmail.com>
2015-04-27 16:24:09 -0400

    export results mailer

---
+++ app/mailers/exports_mailer.rb
@@ -0,0 +1,13 @@
+class ExportsMailer < ActionMailer::Base
+  default :from => 'CWW@taskforce.org'
+
+  def export_results(user, site_id, format)
+    s3 = AWS::S3.new(region: 'us-east-1')
+    aws_object_key = DataExporter.aws_file_key(site_id, format)
+    aws_object = s3.bucket(ENV['S3_BUCKET_NAME'])[aws_object_key]
+    
+    @results_url = aws_object.public_url
+    
+    mail(:to => user.email, :subject => "Results for your data request")
+  end
+end
