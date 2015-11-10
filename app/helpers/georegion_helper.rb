9958447017946ebdd674073f647ee1f953b506b7
vitchell <mdlane@alumni.clemson.edu>
2015-11-10 15:19:00 -0500

    Active projects

--- app/helpers/georegion_helper.rb
+++ app/helpers/georegion_helper.rb
@@ -1,9 +1,9 @@
 module GeoregionHelper
   def georegion_projects_list_subtitle
     if @filter_by_category
-      pluralize(@georegion_projects_count, "#{@category_name} project", "#{@category_name} projects") + " in #{@area.name.capitalize}"
+      pluralize(@georegion_projects_count, "#{@category_name} active project", "#{@category_name} active projects") + " in #{@area.name.capitalize}"
     else
-      "#{pluralize(@georegion_projects_count, 'project', 'projects')} in #{@area.name.capitalize}"
+      "#{pluralize(@georegion_projects_count, 'active project', 'active projects')} in #{@area.name.capitalize}"
     end
   end
 
