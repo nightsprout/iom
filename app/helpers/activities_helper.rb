dfc2ba2d9a3737d988d634352a593963c6577c86
Daniel Powell <dtpowl@gmail.com>
2016-02-17 15:09:29 -0500

    fix helpers for project counts in header text

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,14 +1,14 @@
 module ActivitiesHelper
   def activities_projects_list_subtitle
     if @filter_by_location
-      pluralize(@activity_project_count, "#{@data.name} project", "#{@data.name} projects") + " in #{@location_name}"
+      pluralize(@project.total_entries, "#{@data.name} project", "#{@data.name} projects") + " in #{@location_name}"
     else
        location = if @site.navigate_by_country?
          pluralize(@data.total_countries(@site), 'country', 'countries')
        else
          pluralize(@data.total_regions(@site), @site.word_for_regions.singularize, @site.word_for_regions)
        end
-       pluralize(@activity_project_count, "#{@data.name} project", "#{@data.name} projects") + " in #{location}"
+       pluralize(@project.total_entries, "#{@data.name} project", "#{@data.name} projects") + " in #{location}"
     end
   end
 
--- app/helpers/georegion_helper.rb
+++ app/helpers/georegion_helper.rb
@@ -1,9 +1,9 @@
 module GeoregionHelper
   def georegion_projects_list_subtitle
     if @filter_by_category
-      pluralize(@georegion_projects_count, "#{@category_name} active project", "#{@category_name} active projects") + " in #{@area.name.capitalize}"
+      pluralize(@projects.total_entries, "#{@category_name} active project", "#{@category_name} active projects") + " in #{@area.name.capitalize}"
     else
-      "#{pluralize(@georegion_projects_count, 'active project', 'active projects')} in #{@area.name.capitalize}"
+      "#{pluralize(@projects.total_entries, 'active project', 'active projects')} in #{@area.name.capitalize}"
     end
   end
 
